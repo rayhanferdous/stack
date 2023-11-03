29394236e5802b2701ff706e75c27f682425fc9b
Rayhan Ferdous <107350448+rayhanferdous@users.noreply.github.com>
2023-11-03 20:43:22 +0600

    modify icon btn

--- src/components/utils/IconBtn.tsx
+++ src/components/utils/IconBtn.tsx
@@ -9,7 +9,7 @@ type Props = {
 
 const IconBtn = ({ title, icon }: Props) => {
   return (
-    <span className="inline-flex items-center px-7 py-4 gap-3 bg-stk-slate-2 rounded-2xl ">
+    <button className="inline-flex items-center px-7 py-4 gap-3 bg-stk-slate-2 rounded-2xl ">
       <Image
         width={25}
         height={25}
@@ -17,7 +17,7 @@ const IconBtn = ({ title, icon }: Props) => {
         alt="stack-dropdown"
       />
       <p className="text-stk-gray-2 text-xs font-medium">{title}</p>
-    </span>
+    </button>
   );
 };
 
